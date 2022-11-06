8fa0dd34840f2ed2a1d56fbcb19a1428f971639c
SebastianSoftware <56850980+SebastianSoftware@users.noreply.github.com>
2022-11-06 10:23:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+let loaddependenciestime1 = Date.now();
+console.log('[DEBUG] Loading Dependencies...');
+var mysql = require('mysql');
+let loaddependenciestime2 = Date.now();
+let elapsedloaddependencies = loaddependenciestime1 - loaddependenciestime2;
+console.log('[DEBUG] Loaded Dependencies. (' + elapsedloaddependencies + 'ms).');
